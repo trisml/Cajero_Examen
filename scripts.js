21479d93e856b4bddb755f5349de16b9c064f105
Tristán Marín <tristanmarinlopez@alumnos.tuniversformacion.es>
2023-10-30 08:52:05 +0100

    Estructura del js lista

--- scripts.js
+++ scripts.js
@@ -1,6 +1,30 @@
 document.addEventListener("DOMContentLoaded",() => {
-    
 
+    //Vinculamos html y js
+    const btnDepositar = document.getElementById("depositar");
+    const btnRetirar = document.getElementById("retirar");
+    const btnTransferir = document.getElementById("transferir");
+    const btnCambiarPin = document.getElementById("cambiarPin");
+    const btnSalir = document.getElementById("salir");
+    const mensaje = document.getElementById("mensaje");
+
+    //Hacemos la firma de las funciones
+
+    function depositar(){
+
+    }
+    function retirar(){
+
+    }
+    function transferir(){
+
+    }
+    function cambiarPin(){
+
+    }
+    function salir(){
+        
+    }
 
 
 
