d96caf652d48c4e58aa67b43072183e7469edf8f
Tristán Marín <tristanmarinlopez@alumnos.tuniversformacion.es>
2023-10-30 09:12:18 +0100

    Funciones principales acabadas

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded",() => {
 
+    let saldo = 1000;
+    const PIN_CORRECTO = 1111;
+    let intentosRestantes = 5;
+
+
     //Vinculamos html y js
     const btnDepositar = document.getElementById("depositar");
     const btnRetirar = document.getElementById("retirar");
@@ -10,11 +15,45 @@ document.addEventListener("DOMContentLoaded",() => {
 
     //Hacemos la firma de las funciones
 
-    function depositar(){
+    function mostrarSaldo(){
+        mensaje.innerHTML = `Su saldo actual es de : ${saldo.toFixed(2)}`
 
     }
-    function retirar(){
+    function iniciarSesion(){
+        let pin = parseFloat(prompt("Introduzca el pin de su cuenta bancaria"))
+        while(pin !== PIN_CORRECTO && intentosRestantes > 1){
+            intentosRestantes--;
+            alert(`El pin introducido es incorrecto, intentelo de nuevo. ${intentosRestantes} intentos restantes`)
+        }if(pin === PIN_CORRECTO){
+            alert("El pin introducido es correcto. Bienvenido")
+            mostrarSaldo();
 
+        }else{
+            window.location.href = "./templates/cajeroBloqueado.html"
+        }
+
+
+
+
+
+    }
+    function depositar(){
+        const deposito = parseFloat(prompt("Introduzca la cantidad a depositar"))
+        if(deposito !== isNan || deposito > 0){
+            saldo += deposito
+            mostrarSaldo()
+         }else{
+            alert("Cantidad invalida, compruebelo e intentelo de nuevo")
+         }
+    }
+    function retirar(){
+        const retiro = parseFloat(prompt("Introduzca la cantidad a retirar"))
+        if(retiro !== isNan || retiro > 0 || retiro < saldo){
+            saldo -= retiro;
+            mostrarSaldo();
+        }else{
+            alert("Cantidad invalida, compruebelo e intentelo de nuevo")
+        }
     }
     function transferir(){
 
@@ -23,7 +62,7 @@ document.addEventListener("DOMContentLoaded",() => {
 
     }
     function salir(){
-        
+
     }
 
 
